50e93546e4b845b6d75fdc78a52d8e810156855f
AKY07 <imaalokd7@gmail.com>
2021-09-28 18:56:05 +0530

    Datatypes

---
+++ app.js
@@ -0,0 +1,35 @@
+// console.log("Hello World")
+// console.warn("Hello Warning")
+// console.error("Hello error")
+
+
+// DataTypes
+
+//String
+//console.log("Hello World")  //Hello World
+//console.log(typeof"Hello World")  //string
+//console.log(typeof"d")   //string - in js all character is   string.
+
+// Number
+// console.log(typeof 1)  //number
+// console.log(typeof 1.2)  //number
+// console.log(typeof 112234455666)  //number
+// console.log(typeof -1)  //number
+// console.log(typeof -1.2)  //number
+// console.log(typeof -123445456656)  //number
+
+//boolean
+// console.log(true)   //true
+// console.log( typeof true) //boolean
+// console.log(false)  //false
+// console.log(typeof false)  //boolean
+
+
+
+
+
+
+
+
+
+
