b800018c28bda7ddadf564be5d42002410a93834
AKY07 <imaalokd7@gmail.com>
2021-09-29 19:15:13 +0530

    DOM API SELECTION METHODS

--- app.js
+++ app.js
@@ -1,54 +1,28 @@
-//Operators
+var heading=document.getElementById('heading')
+heading.innerHTML="Hi from JS"
+var para=document.getElementsByClassName('para')
+console.log(heading)
+console.log(typeof heading)  //object
+console.log(para)
+console.log(para.length) //2
+console.log(para[0])
+console.log(para[1])
 
-//Arithmetic Operators
-a=10
-b=5
-console.log('a+b=',a+b)
-console.log('b-a=',b-a)
-console.log('b/a=',b/a)
-console.log('b*a=',b*a)
-console.log('b**a=',b**a)
-console.log('b%a=',b%a)
+// para.push('aalok')
 
-//Assignment Operators
-console.log('b ',b) //b 5
-b += 10
-console.log('b+=10',b) // b+=10 15
-b++
-console.log('b++',b) // b++ 16
-b-=10
-console.log('b-=10',b) //b-=10 6
-b--
-console.log('b--',b) // b-- 5
+var tags=document.getElementsByTagName('a')
+console.log(tags) //htmlcollection
 
-//Comparison operators
+var query=document.querySelector('#heading')
+console.log(query)  //h2#heading
+var query=document.querySelector('.para')
+console.log(query)  //p.para
 
-var d =10
-var e =2
-var f="10"
-console.log(d>e)
-console.log(d<e)
-console.log(d>=e)
-console.log(d<=e)
-console.log(d==e)
-console.warn(d==f)  //true  - double equal to is lose check, it does'nt check types
-console.warn(d===f) //false - triple equal also check for type
 
-console.warn(d+f)  //1010
 
-//Logical Operators
 
-//AND-&&
-console.log(true&&true)  //true
-console.log(true&&false)  //false
-console.log(false&&true)  //false
-console.log(false&&false)  //false
 
-//OR-||
-console.log(true||true)  //true
-console.log(true||false)  //true
-console.log(false||true)  //true
-console.log(false||false)  //false
+
 
 
 
