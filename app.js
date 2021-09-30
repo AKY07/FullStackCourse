55dbfc1d325e025ac0e4e31e7da428d51fd9ff21
AKY07 <imaalokd7@gmail.com>
2021-09-30 19:12:41 +0530

    Functions

--- app.js
+++ app.js
@@ -1,19 +1,95 @@
-var list=document.querySelector('ul')
-console.log(list)
+//Functions-Block of code does specific task.
 
-var arr=['Go the GYM','cook oamteal','Eat']
-arr.push('complete the assignment')
+// function greet(name='sam',age='32'){
+//     console.log(name,age)
+//     console.log('Hello')
+//     console.log('Hey '+name+' Your age is '+age)
+    
+// }
 
-for(var i=0;i<arr.length;i++){
-    var element=document.createElement('li')
-    var textNode=document.createTextNode(arr[i])
-    // console.log(textNode)
-    // console.log(arr[i])
-    // console.log(element)
-    element.appendChild(textNode)
-    list.appendChild(element)
-    // console.log(element)
+// greet("Aalok",21)
+//output
+// Aalok 21
+// Hello
+// Hey Aalok Your age is 21
+
+// greet()
+//output
+// sam 32
+// Hello
+// Hey sam Your age is 32
+
+
+
+
+
+// function add(a=0,b=0){
+//     // console.log(a+b)
+//     var sum=a+b
+//     return sum
+// }
+
+// sum(10,19)  //29
+// sum(10)  //NaN - not a null
+// console.log(sum)   //refernece error
+// var result=add(10,20)
+// console.log(result)  //30
+
+
+
+//Function Expression
+
+// var add=function(a=0,b=0){
+    
+//     var sum=a+b
+//     return sum
+// }
+
+// console.log(add(10,20))   //30
+
+
+
+
+// var greet=function(name='sam',age='32'){
+//     console.log(name,age)
+//     console.log('Hello')
+//     console.log('Hey '+name+' Your age is '+age)
+    
+// }
+
+// greet("Aalok",21) 
+// //output
+// // Aalok 21
+// // Hello
+// // Hey Aalok Your age is 21
+
+// greet()
+// //output
+// // sam 32
+// // Hello
+// // Hey sam Your age is 32
+
+
+
+
+//Arrow Function
+
+var greet=(name='sam',age='32')=>{
+    console.log(name,age)
+    console.log('Hello')
+    console.log('Hey '+name+' Your age is '+age)
+    
 }
+
+greet("Aalok",21)
+// //output
+// // Aalok 21
+// // Hello
+// // Hey Aalok Your age is 21
+
+
+
+
 
 
 
