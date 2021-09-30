19368d5af59474bcb7501b896a1ca27eba49900f
AKY07 <imaalokd7@gmail.com>
2021-09-30 19:23:44 +0530

    Arrow Functions

--- app.js
+++ app.js
@@ -1,91 +1,25 @@
-//Functions-Block of code does specific task.
 
-// function greet(name='sam',age='32'){
-//     console.log(name,age)
-//     console.log('Hello')
-//     console.log('Hey '+name+' Your age is '+age)
+//below both functions are same
+
+var add=(a=0,b=0)=> a+b
+console.log(add(10,20))  //30
+
+//or
+
+var add=function(a=0,b=0){
     
-// }
-
-// greet("Aalok",21)
-//output
-// Aalok 21
-// Hello
-// Hey Aalok Your age is 21
-
-// greet()
-//output
-// sam 32
-// Hello
-// Hey sam Your age is 32
-
-
-
-
-
-// function add(a=0,b=0){
-//     // console.log(a+b)
-//     var sum=a+b
-//     return sum
-// }
-
-// sum(10,19)  //29
-// sum(10)  //NaN - not a null
-// console.log(sum)   //refernece error
-// var result=add(10,20)
-// console.log(result)  //30
-
-
-
-//Function Expression
-
-// var add=function(a=0,b=0){
-    
-//     var sum=a+b
-//     return sum
-// }
-
-// console.log(add(10,20))   //30
-
-
-
-
-// var greet=function(name='sam',age='32'){
-//     console.log(name,age)
-//     console.log('Hello')
-//     console.log('Hey '+name+' Your age is '+age)
-    
-// }
-
-// greet("Aalok",21) 
-// //output
-// // Aalok 21
-// // Hello
-// // Hey Aalok Your age is 21
-
-// greet()
-// //output
-// // sam 32
-// // Hello
-// // Hey sam Your age is 32
-
-
-
-
-//Arrow Function
-
-var greet=(name='sam',age='32')=>{
-    console.log(name,age)
-    console.log('Hello')
-    console.log('Hey '+name+' Your age is '+age)
-    
+    var sum=a+b
+    return sum
 }
 
-greet("Aalok",21)
-// //output
-// // Aalok 21
-// // Hello
-// // Hey Aalok Your age is 21
+console.log(add(10,20))  //30
+
+
+const greet = name => 'hi ' + name
+console.log(greet('Aalok'))   //hi Aalok
+    
+   
+
 
 
 
