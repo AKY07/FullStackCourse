362a3a4bbe50df6bbf381e61623002dfde9bf475
AKY07 <imaalokd7@gmail.com>
2021-09-30 20:17:38 +0530

    Dark Mode Concept

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
+var body = document.querySelector('body')
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
 
-// const callbackfunc=()=>{
-//     console.log('sam')
-// }
-
-// button.addEventListener('click',callbackfunc)
-
-// const callbackfunc=(event)=>{
-//     console.log(event)
-// }
-
-// button.addEventListener('click',callbackfunc)
-
-
-// const callbackfunc=(event)=>{
-//     console.log(event.target)
-//     console.log(input.value)
-// }
-
-// button.addEventListener('click',callbackfunc)
 
 
 const callbackfunc=(event)=>{
-    // console.log(button.innerHTML)
-    const inputValue=input.value
-    const element=document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
-   
+   body.classList.toggle('dark')
 }
 
 button.addEventListener('click',callbackfunc)
