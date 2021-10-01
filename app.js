b7b65afae400a169a598ca5d78abd273a5b4bc02
AKY07 <imaalokd7@gmail.com>
2021-10-01 18:33:06 +0530

    Basic Calculator

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+// const a = document.getElementsByTagName('input')[0]
+// const b = document.getElementsByTagName('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const resultBox = document.querySelector('.result')
 
+// console.log(a,b)
+// console.log(add,multiply)
+// console.log(result)
 
-
-const callbackfunc=(event)=>{
-   body.classList.toggle('dark')
+const sum=()=>{
+   // const result = a.value+b.value
+   const result = parseInt(a.value)+parseInt(b.value)
+   console.log(result)
+   resultBox.innerHTML=result
+   // resultBox.textContent=result 
 }
+add.addEventListener('click',sum)
 
-button.addEventListener('click',callbackfunc)
+const product=()=>{
+   const result = parseInt(a.value)*parseInt(b.value)
+   console.log(result)
+   resultBox.innerHTML=result
+}
+multiply.addEventListener('click',product)
+
 
 
    
