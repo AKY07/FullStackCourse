85a676d6858d31928c67aac4b355e155eab1e485
AKY07 <imaalokd7@gmail.com>
2021-10-01 19:12:12 +0530

    Basic Calculator-2

--- app.js
+++ app.js
@@ -4,27 +4,72 @@ const b = document.querySelectorAll('input')[1]
 // const b = document.getElementsByTagName('input')[1]
 const add = document.querySelector('#add')
 const multiply = document.querySelector('#multiply')
+const substract = document.querySelector('#substarct')
 const resultBox = document.querySelector('.result')
 
 // console.log(a,b)
 // console.log(add,multiply)
 // console.log(result)
 
-const sum=()=>{
-   // const result = a.value+b.value
-   const result = parseInt(a.value)+parseInt(b.value)
-   console.log(result)
-   resultBox.innerHTML=result
-   // resultBox.textContent=result 
-}
-add.addEventListener('click',sum)
+// const sum=()=>{
+//    // const result = a.value+b.value
+//    const result = parseInt(a.value)+parseInt(b.value)
+//    console.log(result)
+//    resultBox.innerHTML=result
+//    // resultBox.textContent=result 
+// }
+// add.addEventListener('click',sum)
 
-const product=()=>{
-   const result = parseInt(a.value)*parseInt(b.value)
-   console.log(result)
-   resultBox.innerHTML=result
-}
-multiply.addEventListener('click',product)
+// const product=()=>{
+//    const result = parseInt(a.value)*parseInt(b.value)
+//    console.log(result)
+//    resultBox.innerHTML=result
+// }
+// multiply.addEventListener('click',product)
+
+
+
+
+const calculate=(event,operation)=>{
+   // console.log(event.target)
+   // if(operation=='add'){
+   //    console.log('adding')
+   // }
+   // else if(operation=='multiply'){
+   //    console.log('multiply')
+   // }
+   switch(operation){
+      case "add":
+         resultBox.innerHTML=parseInt(a.value)+parseInt(b.value)
+         break
+      case "multiply":
+         resultBox.innerHTML=parseInt(a.value)*parseInt(b.value)
+         break
+      case "substract":
+         resultBox.innerHTML=parseInt(a.value)-parseInt(b.value)
+         break
+      default:
+         resultBox.innerHTML="Not a valid operation"   
+         
+   }
+ 
+ }
+
+add.addEventListener('click',(event) =>{ 
+   return calculate(event,'add')
+})
+
+// add.addEventListener('click',() =>  calculate('add'))
+
+multiply.addEventListener('click',(event) =>{
+   return calculate(event,'multiply')
+   
+})
+
+substract.addEventListener('click',(event) =>{
+   return calculate(event,'substract')
+   
+})
 
 
 
