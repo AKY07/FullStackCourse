fa265c618108e8466e0827f0fc5722dd340dda3b
AKY07 <imaalokd7@gmail.com>
2021-10-04 19:52:01 +0530

    Promise intro

--- app.js
+++ app.js
@@ -1,175 +1,21 @@
-// let a=20
-// var b=40
-// console.log(a,b)  // 20 40
-// a=10
-// b=20
-// console.log(a,b)  //10,20
+// setTimeout(()=>{
+//     console.log("aalok")
+// },5000)  //after 5 sec output show ,5000ms
+
+// console.log("sam")  //sam
+
+// //here sam show first then aalok even if time is 0, also even if sam is 100 or more time
 
 
-// let vs var-let has block scope but var has global scope
+//Promise
 
-// if(true){
-//     var Name='Ferrari'
-//     console.log(Name)    //Ferrari
-// }
+var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
 
-// console.log(Name)       //Ferrari
-
-// if(true){
-//     let Name='Ferrari'
-//     console.log(Name)    //Ferrari
-// }
-
-// console.log(Name)       //error
-
-
-// for(var i=0;i<10;i++){
-//     console.log('loop ran')
-// }
-// console.log(i)   //10
-
-// for(let i=0;i<10;i++){
-//     console.log('loop ran')
-// }
-// console.log(i)   //error
-
-
-// if (true){
-//     var Name="Ferrari"
-//     if (true){
-//         console.log(Name)   //Ferrari
-
-//     }  
-// }
-
-// if (true){
-//     console.log(Name)   //Ferrari
-  
-//     if (true){
-//         var Name="Ferrari" 
-        
-//     }  
-// }
-
-
-
-// const print=()=>{
-//     var Name='Aalok'
-//     if(true){
-//         console.log(Name)
-//     }
-//     return print
-// }
-
-
-
-// console.log(this)   //window object
-// console.log(this.alert('hi'))
-
-
-// let car={}
-// console.log(car)  //{}
-// console.log(typeof car)  //object
-
-
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(){
-//         console.log(name +'by'+this.manufacturer)
-//     }
-// }
-
-// car.print()    //FerraribyMercedes
-// console.log(car.name)  //C Class
-// console.log(car.manufacturer)   //Mercedes
-
-//A function inside a object is a method
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(a){
-//         console.log(a)
-//     }
-// }
-
-// car.print('Ferrari')   //Ferrari
-
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(){
-//       console.log(this)
-//     }
-// }
-
-// car.print()
-
-// let manufacturer="ferrari"
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(){
-//       console.log(manufacturer)
-//     }
-// }
-// car.print()  //ferrari
-
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(){
-//       console.log(car.manufacturer,car.name)
-//     }
-// }
-
-// car.print()    //Mercedes C Class
-
-
-// let car = {
-//     name: "C Class",
-//     manufacturer: "Mercedes",
-//     print: function () {
-//         console.log(`${this.name} was created by ${this.manufacturer}`)
-//     }
-// }
-
-// car.print()    //C Class was created by Mercedes
-
-
-
-// let car = {
-//     name: "C Class",
-//     manufacturer: "Mercedes",
-//     print:()=> {
-//         console.log(this)
-//         console.log(`${this.name} was created by ${this.manufacturer}`)
-//     }
-// }
-
-// car.print()  //Ferrari was created by undefined
+console.log(data)   //Promise {<pending>}
 
 
 
 
-
-let car = {
-    name: "C Class",
-    manufacturer: "Mercedes",
-    print:()=> {
-        console.log(this)
-        console.log(`${this.name} was created by ${this.manufacturer}`)
-    }
-}
-//console.log(Object)   // Object() { [native code] }
-//console.log(car.hasOwnProperty('name'))   //true
-//console.log(car)   //{name: 'C Class', manufacturer: 'Mercedes', print: ƒ}
-// console.log(Object.keys(car))     //(3) ['name', 'manufacturer', 'print']
-// console.log(Object.values(car))  //(3) ['C Class', 'Mercedes', ƒ]
 
 
 
